46d5013036058a9a0499f1492d41b3957883d7b5
CarevCho <hyeonee00@gmail.com>
2018-07-25 22:43:26 +0900

    # inverse print asterisk with input natural number

--- baekjoon/for_loop/for_loop_6_asterisk_3.cpp
+++ baekjoon/for_loop/for_loop_6_asterisk_3.cpp
@@ -1,17 +1,14 @@
 /*
 문제
-첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제
-
-하지만, 오른쪽을 기준으로 정렬한 별 (예제 참고)을 출력하시오.
+첫째 줄에는 별 N개, 둘째 줄에는 별 N-1개, ..., N번째 줄에는 별 1개를 찍는 문제
 
 입력
 첫째 줄에 N (1<=N<=100)이 주어진다.
 
 출력
 첫째 줄부터 N번째 줄 까지 차례대로 별을 출력한다.
-https://www.acmicpc.net/problem/2439
+https://www.acmicpc.net/problem/2440
 */
-/*
 #include <iostream>
 
 using std::cout;
@@ -28,14 +25,10 @@ int main(int argc, char* argv[])
 
 	// print out asterisk
 	for (int i = 0; i < iN; i++) {
-		for (int j = 0; j < iN; j++) {
-			if(j < iN-i-1)
-				cout << ' ';
-			else
-				cout << "*";
+		for (int j = 0; j < iN-i; j++) {	
+			cout << "*";
 		}cout << "\n";
 	}
 
 	return 0;
 }
-*/
