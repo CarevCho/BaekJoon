3e7b26e7b37af75fe575079309a45a099b44c7fd
CarevCho <hyeonee00@gmail.com>
2018-07-25 22:39:53 +0900

    # print asterisk with right alignment

--- baekjoon/for_loop/for_loop_5_asterisk_2.cpp
+++ baekjoon/for_loop/for_loop_5_asterisk_2.cpp
@@ -2,13 +2,15 @@
 문제
 첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제
 
+하지만, 오른쪽을 기준으로 정렬한 별 (예제 참고)을 출력하시오.
+
 입력
 첫째 줄에 N (1<=N<=100)이 주어진다.
 
 출력
 첫째 줄부터 N번째 줄 까지 차례대로 별을 출력한다.
+https://www.acmicpc.net/problem/2439
 */
-/*
 #include <iostream>
 
 using std::cout;
@@ -25,11 +27,13 @@ int main(int argc, char* argv[])
 
 	// print out asterisk
 	for (int i = 0; i < iN; i++) {
-		for (int j = 0; j < i+1; j++) {
-			cout << "*";
+		for (int j = 0; j < iN; j++) {
+			if(j < iN-i-1)
+				cout << ' ';
+			else
+				cout << "*";
 		}cout << "\n";
 	}
 
 	return 0;
 }
-*/
