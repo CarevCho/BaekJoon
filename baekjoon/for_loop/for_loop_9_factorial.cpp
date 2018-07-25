874cfc6d17502c7d9f0e29aa2a48ca3fe7fb3e57
CarevCho <hyeonee00@gmail.com>
2018-07-26 01:45:05 +0900

    # Sum input serial number

--- baekjoon/for_loop/for_loop_9_factorial.cpp
+++ baekjoon/for_loop/for_loop_9_factorial.cpp
@@ -8,6 +8,7 @@ n이 주어졌을 때, 1부터 n까지 합을 구하는 프로그램을 작성하시오.
 출력
 1부터 n까지 합을 출력한다.
 */
+/*
 #include <iostream>
 
 int main(int argc, char* argv[])
@@ -23,3 +24,4 @@ int main(int argc, char* argv[])
 	
 	return 0;
 }
+*/
